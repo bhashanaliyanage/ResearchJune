696afa3fca5c4b7b89f6f71950d92fd445bfdd92
ashmhmd25321 <ashfak25321@gmail.com>
2023-01-06 00:12:12 +0530

    last

--- app/src/main/java/com/example/image_detector/ChatBot.java
+++ app/src/main/java/com/example/image_detector/ChatBot.java
@@ -3,6 +3,12 @@ package com.example.image_detector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.samsao.messageui.views.MessagesWindow;
 
 public class ChatBot extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class ChatBot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
+
+        // "context" must be an Activity, Service or Application object from your app.
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+
+        MessagesWindow messagesWindow = (MessagesWindow) findViewById(R.id.customized_messages_window);
+        EditText message = messagesWindow.getWritingMessageView().findViewById(com.samsao.messageui.R.id.message_box_text_field);
+
+        message.setHint("Type Here...");
+        messagesWindow.setBackgroundResource(com.google.android.material.R.color.design_default_color_primary_dark);
+
+        messagesWindow.getWritingMessageView().findViewById(com.samsao.messageui.R.id.message_box_button).setOnClickListener(v -> {
+            messagesWindow.sendMessage(message.getText().toString());
+
+            Python py = Python.getInstance();
+
+            PyObject pyObj = py.getModule("chatbot");
+
+            PyObject obj = pyObj.callAttr("main", message.getText().toString());
+
+            messagesWindow.receiveMessage(obj.toString());
+
+            message.setText("");
+        });
+
     }
 }
--- app/src/main/python/chatbot.py
+++ app/src/main/python/chatbot.py
@@ -1,2 +1,59 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+
 def main(inputData):
-    return ""+str(inputData)
+    # Creating Pairs
+    pairs = [
+        [
+            r"my name is (.*)",
+            ["Hello %1, How are you doing today ?", ]
+        ],
+        [
+            r"hi|hey|hello",
+            ["Hello", "Hey there", ]
+        ],
+        [
+            r"what is your name?|Who are you?",
+            ["You can call me 'Ancient One'", ]
+        ],
+        [
+            r"how are you ?",
+            ["I am fine, thank you! How can i help you?", ]
+        ],
+        [
+            r"I am fine, thank you",
+            ["great to hear that, how can i help you?", ]
+        ],
+        [
+            r"how can i help you? ",
+            ["i am looking for online guides and courses to learn data science, can you suggest?",
+             "i am looking for data science training platforms", ]
+        ],
+        [
+            r"i'm doing good",
+            ["That's great to hear", "How can i help you?:)", ]
+        ],
+        [
+            r"i am looking for online guides and courses to learn data science, can you suggest?",
+            ["Pluralsight is a great option to learn data science. You can check their website", ]
+        ],
+        [
+            r"thanks for the suggestion. do they have great authors and instructors?",
+            ["Yes, they have the world class best authors, that is their strength;)", ]
+        ],
+        [
+            r"(.*) thank you so much, that was helpful",
+            ["Iam happy to help", "No problem, you're welcome", ]
+        ],
+        [
+            r"quit|bye",
+            ["Bye, take care. See you soon :) ", "It was nice talking to you. See you soon :)"]
+        ],
+    ]
+
+    chat = Chat(pairs, reflections)
+
+    outputData = chat.respond(inputData)
+
+    return "" + str(outputData)
